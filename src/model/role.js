c8691fe8f7c0bb39c9b9fe29324369fd6f31a544
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-03 11:17:10 -0800

    added commented post for roles

--- src/model/role.js
+++ src/model/role.js
@@ -7,5 +7,15 @@ const roleSchema = new mongoose.Schema({
   capabilities: {type: Array, required:true},
 });
 
+// router.post('/roles', (req, res, next) => {
+//   let saved = [];
+//   Object.keys(capabilities).map(role => {
+//     let newRecord = new Role({type: role, capabilities: capabilities[role]});
+//     saved.push(newRecord.save());
+//   });
+//   Promise.all(saved);
+//   res.send('Roles Created');
+// });
+
 
 module.exports = mongoose.model('roles', roleSchema)
