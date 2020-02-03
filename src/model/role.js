57b7734dcf6e04119f848e68a6b96e3b6cd0dab6
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-03 10:00:27 -0800

    added capabilities and dependencies in roles.js

--- src/model/role.js
+++ src/model/role.js
@@ -2,3 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bycrpt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const capabilities = {
+  admin: ['create','read','update','delete', 'superuser'],
+  user: ['read'],
+};
