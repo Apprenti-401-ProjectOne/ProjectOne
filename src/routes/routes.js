9bc199b96c20bae9efcd897863137e6eadd7c9de
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-02-03 11:02:34 -0800

    modularized app.js for mongoose connection and instantiated user in post route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 
 const bearer = require('../authmiddleware/bearer');
-const user = require('../model/user');
+const User = require('../model/user');
 
 
 router.post('/signup', (req, res) => {
+  console.log(req.body)
+  let user = new User(req.body);
   user.save(req.body)
-  .then(results => {
-    res.status(200);
-    res.send('User Created');
-    
-  });
-
+    .then(results => {
+      res.status(200);
+      res.send('User Created');
+    })
+    .catch(error => console.error(error))
 
 });
 
@@ -19,4 +20,4 @@ router.post('/signin', (req, res) => {
 
 });
 
-
+module.exports = router;
