28b64711cf2564e3f9df8e3d99db3b4f8020bee6
travis skyles <travis.skyles@gmail.com>
2020-02-04 13:33:38 -0800

    basic auth test

---
+++ __tests__/basicAuth.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.env.SECRET='test';
+
+const supergoose = require('./supergoose');
+const basic = require('../src/authmiddleware/basic');
+const User = require('../src/model/user');
+
+let users = {
+  admin: {username: 'admin', password: 'password', email: 'admin@admin.com', role: 'admin'},
+  editor: {username: 'editor', password: 'password', email: 'editor@editor.com', role: 'editor'},
+  user: {username: 'user', password: 'password', email: 'user@user.com', role: 'user'},
+};
+
+beforeAll(async (done) => {
+  await supergoose.startDB();
+  const adminUser = await new User(users.admin).save();
+  const editorUser = await new User(users.editor).save();
+  const userUser = await new User(users.user).save();
+  done();
+});
+
+afterAll(supergoose.stopDB);
+
+describe('Auth Middleware', () => {
+
+  // admin:password: YWRtaW46cGFzc3dvcmQ=
+  // admin:foo: YWRtaW46Zm9v
+
+  let errorObject = 'Invalid User ID/Password';
+
+
+  it('fails a login for a user (admin) with the incorrect basic credentials', () => {
+    let req = {
+      headers: {
+        authorization: 'Basic YWRtaW46Zm9v',
+      },
+    };
+    let res = {};
+    let next = jest.fn();
+
+    return basic(req, res, next)
+      .then(() => {
+        expect(next).toHaveBeenCalledWith(errorObject);
+      });
+  });
+});
