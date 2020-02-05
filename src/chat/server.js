71397cc4f4bd4346646f2675fdf06b48d3c98b6f
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-05 00:28:28 -0800

    chat rooms working

--- src/chat/server.js
+++ src/chat/server.js
@@ -7,3 +7,8 @@ let chat = io.connect('http://localhost:3001/chat');
 chat.on('welcome', (message) => {
   console.log('Recieved: ', message);
 });
+
+chat.emit('joinRoom', 'Job Chat Rm 1');
+
+chat.on('err', (err) => console.log(err));
+chat.on('success', (res) => console.log(res));
