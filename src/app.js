d9f9f1ad97e64fa1528e710ab4a34767eb0d8a80
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-05 08:15:30 -0800

    root files

--- src/app.js
+++ src/app.js
@@ -13,7 +13,46 @@ const logger = require('./middleware/logger');
 const timestamp = require('./middleware/timestamp');
 // const sendEmail = require('./middleware/email.js');
 
-//Middleware
+//chat feature
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+const port = 3001;
+
+// io.on('connection', (socket) => {
+//   socket.emit('Hello', 'Welcome to the Chat Server');
+//   console.log('There is a new connection');
+// });
+
+//chat rooms
+const chatRooms = ['Job Chat Rm 1', 'Job Chat Rm 2'];
+
+
+//chat namespace
+io
+  .of('/chat')
+  .on('connection', (socket) => {
+
+    socket.emit('Welcome', 'You are in the chat namespace');
+    console.log('Chat Namespace');
+
+    socket.on('joinRoom', (room) => {
+      if (chatRooms.includes(room)) {
+        socket.join(room);
+        io
+          .of('/chat')
+          .in(room).emit('newUser', 'Someone new wants to chat about this job');
+        return socket.emit('success', 'You have joined the chat for this job.');
+      } else {
+        return socket.emit('err', 'No chat has been started for this job yet.');
+      }
+    });
+  });
+
+http.listen(port, () => {
+  console.log(`Chat is listening on ${port}`);
+});
+
+//Middleware 
 app.use(logger);
 app.use(timestamp);
 // app.use(sendEmail);
