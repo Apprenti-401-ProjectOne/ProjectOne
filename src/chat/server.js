d2cc509d401596b7a554ec143c4941b748f6ccc8
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-04 23:42:56 -0800

    socket.io connection made

---
+++ src/chat/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const io = require('socket.io-client');
+
+let socket = io.connect('http://localhost:3001');
+
+socket.on('welcome', (data) => {
+  console.log('Recieved: ', data);
+});
